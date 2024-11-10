443627d9172e4e6a5b6ac9a7e7b6b2c98f4f145d
User <User@172.17.79.219>
2024-11-10 20:03:13 +0800

    this is my first commit for lab4q3

---
+++ s75282/src/lab4q3.java
@@ -0,0 +1,25 @@
+
+import javax.swing.JOptionPane;
+public class lab4q3 {
+     public static void main(String[] args) {
+        int currentYear = 2024;
+        String input = JOptionPane.showInputDialog("Enter the IC number: ");
+        
+        String yearPart = input.substring(0,2);
+        String monthPart = input.substring(2,4);
+        String dayPart = input.substring(4,6);
+        int birthYear = Integer.parseInt(yearPart);
+        int month = Integer.parseInt(monthPart);
+        int day = Integer.parseInt(dayPart);
+        birthYear = birthYear + 2000;
+        int age = currentYear - birthYear;
+        
+        String message= String.format ("Birth date %d-%d-%d",day,month,birthYear) +
+                String.format("\nAge %d", age);
+        
+        
+            JOptionPane.showMessageDialog(null, message);
+    }
+}
+
+
