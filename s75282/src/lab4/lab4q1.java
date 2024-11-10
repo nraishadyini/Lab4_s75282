346d4c134c635d51db532ffaa1531ba08c2acaf4
User <User@172.17.79.219>
2024-11-10 10:41:14 +0800

    this is my first commit for lab4q1

---
+++ s75282/src/lab4/lab4q1.java
@@ -0,0 +1,21 @@
+
+package lab4;
+
+import javax.swing.JOptionPane;
+public class lab4q1 {
+        public static void main(String[] args) {
+
+      String input = JOptionPane.showInputDialog("Enter RADIUS: ");
+        double r = Double.parseDouble(input);
+        
+        double diameter = 2*r ;
+        double circumference = 2* 3.14*r;
+        double  area = 3.14*r*r ;
+        
+        
+        JOptionPane.showMessageDialog(null, "the diameter is " + diameter);
+        JOptionPane.showMessageDialog(null, "the circumference is " + circumference);
+        JOptionPane.showMessageDialog(null, "the area is " + area);
+        
+}
+}
